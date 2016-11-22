2a74d2e6a14df7cffe9d0426a1990dc2853650a1
Angela Colter <angela.colter@gsa.gov>
2016-11-22 16:24:06 -0500

    Remove Acq projects from projects list
    Also removed the check on the number of strategists and the percent billable

--- strategy_hours_bulk.R
+++ strategy_hours_bulk.R
@@ -13,8 +13,8 @@ strategy_hours <- function(end_date) {
   strategists <- as.vector(strategists_df[,1])
   strategists_hours <- tock_df[tock_df$employee %in% strategists & tock_df$end_date %in% end_date,]
 
-  # create a list of billable projects from the projects list
-  projects <- projects_df[projects_df$billable %in% "True",]
+  # create a list of billable projects from the projects list that doesn't include Acq projects
+  projects <- projects_df[projects_df$billable %in% "True" & !(projects_df$profit_loss_account %in% "FY17 Acquisition Svcs Billable"),]
   billable_projects <- as.vector(projects[,3])
   billable_hours <- strategists_hours[strategists_hours$project_name %in% billable_projects,]
   
@@ -24,13 +24,6 @@ strategy_hours <- function(end_date) {
   # calculate the total billable hours worked by the strategists
   total_billable_hours <- sum(billable_hours$hours_spent)
   
-  # calculate the percentage billable
-  percent_billable <- total_billable_hours / total_hours
-  
-  # find out how many strategists appear in the Tock data
-  strategists <- as.vector(strategists_df[,1])
-  strategists_in_tock <- levels(droplevels(strategists_hours$employee))
-
   # create csv files for all hours worked and billable hours worked
   write.csv(strategists_hours, file="all_hours.csv")
   write.csv(billable_hours, file="billable_hours.csv")
@@ -38,8 +31,6 @@ strategy_hours <- function(end_date) {
   # print it all out
   print(paste("Total hours worked by strategists:", total_hours))
   print(paste("Billable hours worked by strategists:", total_billable_hours))
-  print(paste("Percentage of billable hours worked:", percent_billable))
-  print(paste("All strategists are in the Tock file:", (length(strategists) == length(strategists_in_tock))))
   
 }
 
