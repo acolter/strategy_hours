c5bc908d8167908029d12488449363d2861b9ea3
Angela Colter <angela.colter@gsa.gov>
2016-11-24 09:34:03 -0500

    Remove quotes around end_date

--- strategy_hours_bulk.R
+++ strategy_hours_bulk.R
@@ -13,21 +13,21 @@ strategy_hours <- function(end_date) {
                              & users_df$current_employee %in% "True",]
   strategists <- as.vector(strategists_df[,1])
   strategists_hours <- tock_df[tock_df$employee %in% strategists 
-                               & tock_df$end_date %in% "end_date",]
+                               & tock_df$end_date %in% end_date,]
   strategists_hours <- strategists_hours[!(strategists_hours$hours_spent %in% "0"),]
-
-  # create a list of billable projects from the projects list that doesn't include Acq projects
+  
+  # create a list of billable projects that don't include acq
   projects <- projects_df[projects_df$billable %in% "True" 
                           & !(projects_df$profit_loss_account %in% "FY17 Acquisition Svcs Billable"),]
   billable_projects <- as.vector(projects[,3])
   billable_hours <- strategists_hours[strategists_hours$project_name %in% billable_projects,]
-  
+
   # calculate the total hours worked by the strategists
   total_hours <- sum(strategists_hours$hours_spent)
   
   # calculate the total billable hours worked by the strategists
   total_billable_hours <- sum(billable_hours$hours_spent)
-  
+ 
   # create csv files for all hours worked and billable hours worked
   write.csv(strategists_hours, file="all_hours.csv")
   write.csv(billable_hours, file="billable_hours.csv")
@@ -35,6 +35,6 @@ strategy_hours <- function(end_date) {
   # print it all out
   print(paste("Total hours worked by strategists:", total_hours))
   print(paste("Billable hours worked by strategists:", total_billable_hours))
-  
-}
+
+  }
 
