3db4eaf09f6ef5c6cec9fbca648861efc0aa6b39
Angela Colter <angela.colter@gsa.gov>
2017-01-03 13:26:20 -0500

    Added chapter_goal variable

--- strategy_hours_bulk.R
+++ strategy_hours_bulk.R
@@ -47,7 +47,7 @@ strategy_hours <- function(end_date) {
   print(paste("Total hours recorded by strategists:", total_hours))
   print(paste("Billable hours recorded by strategists:", total_billable_hours))
   print(paste("Billable percentage:", sprintf("%1.0f%%", 100*percent_billable)))
-  print(paste("Number of hours off chapter goal:", chapter_goal - total_billable_hours))
+  print(paste("Number of hours off chapter goal:", total_billable_hours - chapter_goal))
 
   }
 
