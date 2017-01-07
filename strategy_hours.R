b17681a6a9a027dc11fbf995abeb734726bc39fb
Angela Colter <angela.colter@gsa.gov>
2017-01-07 12:07:52 -0500

    Added chapter goal variable

--- strategy_hours.R
+++ strategy_hours.R
@@ -1,44 +1,52 @@
-strategy_hours <- function() {
+strategy_hours <- function(end_date) {
   
   # download the following files from https://tock.18f.gov/reports/ each time you want to run this function 
   # you'll need to be an authorized user to access the files
   # put them in your working directory
   # read in the files
-  tock_df <- read.csv("2016-11-13.csv") # grab the latest file from "Reports by weekly reporting period"
+  tock_df <- read.csv("slim_timecard_bulk.csv") # from the "Complete timecard data with fewer fields: FY2017" link
   users_df <- read.csv("user_data.csv") # from the "List of all users" link
   projects_df <- read.csv("projects.csv") # from the "List of all projects" link
   
   # create a list of strategists, use that to subset all strategist hours from the Tock data
-  strategists_df <- users_df[users_df$unit %in% "Chapters-Strategists" & users_df$current_employee %in% "True",]
-  strategists <- paste0(as.vector(strategists_df[,1]),"@gsa.gov")
-  all_hours <- tock_df[tock_df$User %in% strategists,]
+  strategists_df <- users_df[users_df$unit %in% "Chapters-Strategists" 
+                             & users_df$current_employee %in% "True",]
+  strategists <- as.vector(strategists_df[,1])
+  strategists_hours <- tock_df[tock_df$employee %in% strategists 
+                               & tock_df$end_date %in% end_date
+                               & !(tock_df$hours_spent %in% "0"),]
   
-  # find out how many strategists appear in the Tock data
-  strategists_in_tock <- levels(droplevels(all_hours$User))
+  # sort the dataframe by employee
+  strategists_hours <- strategists_hours[order(strategists_hours$employee),]
   
-  # create a list of billable projects from the projects list
-  projects <- projects_df[projects_df$billable %in% "True",]
+  # create a list of billable projects that don't include TTS Acq, Federalist or PIF
+  projects <- projects_df[projects_df$billable %in% "True" 
+                          & !(grepl("TTS Acq",projects_df$name)) 
+                          & !(grepl("Federalist",projects_df$name)) 
+                          & !(grepl("PIF",projects_df$name)),]
   billable_projects <- as.vector(projects[,3])
-  billable_hours <- all_hours[all_hours$Project %in% billable_projects,]
-  
+  billable_hours <- strategists_hours[strategists_hours$project_name %in% billable_projects,]
+
   # calculate the total hours worked by the strategists
-  total_hours <- sum(all_hours$Number.of.Hours)
+  total_hours <- sum(strategists_hours$hours_spent)
   
   # calculate the total billable hours worked by the strategists
-  total_billable_hours <- sum(billable_hours$Number.of.Hours)
-
+  total_billable_hours <- sum(billable_hours$hours_spent)
+ 
   # calculate the percentage billable
   percent_billable <- total_billable_hours / total_hours
 
   # create csv files for all hours worked and billable hours worked
-  write.csv(all_hours, file="all_hours.csv")
+  write.csv(strategists_hours, file="all_hours.csv")
   write.csv(billable_hours, file="billable_hours.csv")
   
+  # chapter goal
+  chapter_goal <- 418
+  
   # print it all out
-  print(paste("Total hours worked by strategists:", total_hours))
-  print(paste("Total billable hours worked by strategists:", total_billable_hours))
-  print(paste("Percentage of billable hours worked:", percent_billable))
-  print(paste("All strategists are in the Tock file:", (length(strategists) == length(strategists_in_tock))))
-  
-  
-}
+  print(paste("Total hours recorded by strategists:", total_hours))
+  print(paste("Billable hours recorded by strategists:", total_billable_hours))
+  print(paste("Billable percentage:", sprintf("%1.0f%%", 100*percent_billable)))
+  print(paste("Number of hours off chapter goal:", total_billable_hours - chapter_goal))
+
+  }
